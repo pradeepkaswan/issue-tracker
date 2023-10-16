51d27a37189c1caf8716b0bb6c6138de168f3ab9
pradeepkaswan <pradeepkaswan7@gmail.com>
2023-10-16 22:01:18 +0530

    style: Customize styling for navbar links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,13 @@
+'use client';
+
+import clsx from 'clsx';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { AiFillBug } from 'react-icons/ai';
 
 const NavBar = () => {
+	const currentPath = usePathname();
+
 	const links = [
 		{ label: 'Dashboard', href: '/' },
 		{ label: 'Issues', href: '/issues' },
@@ -15,9 +21,13 @@ const NavBar = () => {
 			<ul className='flex space-x-6'>
 				{links.map((link) => (
 					<Link
-						key={link.href}
-						className='text-zinc-500 hover:text-zinc-800 transition-colors'
 						href={link.href}
+						key={link.href}
+						className={clsx({
+							'text-zinc-900': link.href === currentPath,
+							'text-zinc-500': link.href !== currentPath,
+							'hover:text-zinc-800 transition-colors': true,
+						})}
 					>
 						{link.label}
 					</Link>
